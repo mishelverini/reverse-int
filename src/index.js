a0b9dd62e480f6c98b11b4d9f738e8761d8c7a35
Mikhail Vazhynski <mishelverini@yahoo.com>
2020-03-18 22:16:44 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    return (Math.abs(n) + '').split('').reverse().map(function (element) {
+        return Number.parseInt(element);
+    }).join('');
 }
